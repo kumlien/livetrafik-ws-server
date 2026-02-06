469b9d15f073170ec39378780ee7a5c8f1d0ebc4
Svante Kumlien <svante.kumlien@gmail.com>
2026-02-06 16:07:34 +0100

    Add version info contributor with build and monitoring details
    Added VersionInfoConfig with InfoContributor bean that exposes build properties (artifact, name, group, version, time) and monitoring configuration (serverId, version) through the actuator info endpoint.

--- src/main/java/se/kumliens/livetrafik/config/LivetrafikRuntimeHints.java
+++ src/main/java/se/kumliens/livetrafik/config/LivetrafikRuntimeHints.java
@@ -3,13 +3,16 @@ package se.kumliens.livetrafik.config;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 import se.kumliens.livetrafik.model.VehicleBroadcastPayload;
 
 public class LivetrafikRuntimeHints implements RuntimeHintsRegistrar {
 
     @Override
-    public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+    public void registerHints(@NonNull RuntimeHints hints, @Nullable ClassLoader classLoader) {
+        hints.resources().registerPattern("META-INF/build-info.properties");
         hints.reflection().registerType(VehicleBroadcastPayload.class,
             MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
             MemberCategory.INVOKE_PUBLIC_METHODS,
