42e4f9bb8aa84a81b530787a1a0b129e5f750e70
Svante Kumlien <svante.kumlien@gmail.com>
2026-02-07 07:07:00 +0100

    Add enhanced startup logging with build and monitoring properties
    Moved startup log from main method to @PostConstruct method that includes build version, build time, monitoring version, and server ID. Added constructor injection for MonitoringProperties and BuildProperties with fallback logging when build properties are unavailable.

--- src/main/java/se/kumliens/livetrafik/LivetrafikWsServerApplication.java
+++ src/main/java/se/kumliens/livetrafik/LivetrafikWsServerApplication.java
@@ -1,10 +1,13 @@
 package se.kumliens.livetrafik;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import se.kumliens.livetrafik.config.MonitoringProperties;
 
@@ -14,9 +17,36 @@ import se.kumliens.livetrafik.config.MonitoringProperties;
 @Slf4j
 public class LivetrafikWsServerApplication {
 
+    private final MonitoringProperties monitoringProperties;
+    private final ObjectProvider<BuildProperties> buildPropertiesProvider;
+
+    public LivetrafikWsServerApplication(
+        MonitoringProperties monitoringProperties,
+        ObjectProvider<BuildProperties> buildPropertiesProvider) {
+        this.monitoringProperties = monitoringProperties;
+        this.buildPropertiesProvider = buildPropertiesProvider;
+    }
+
 	public static void main(String[] args) {
-		log.info("Starting LivetrafikWsServerApplication");
 		SpringApplication.run(LivetrafikWsServerApplication.class, args);
 	}
+
+    @PostConstruct
+    void logVersionInformation() {
+        BuildProperties buildProperties = buildPropertiesProvider.getIfAvailable();
+        if (buildProperties != null) {
+            log.info(
+                "Starting LivetrafikWsServerApplication – buildVersion={} buildTime={} monitoringVersion={} serverId={}",
+                buildProperties.getVersion(),
+                buildProperties.getTime(),
+                monitoringProperties.getVersion(),
+                monitoringProperties.getServerId());
+        } else {
+            log.info(
+                "Starting LivetrafikWsServerApplication – monitoringVersion={} serverId={} (build properties unavailable)",
+                monitoringProperties.getVersion(),
+                monitoringProperties.getServerId());
+        }
+    }
 
 }
